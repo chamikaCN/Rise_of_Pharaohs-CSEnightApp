5460af2abc2c02650327537379b7e2ab25bd48f0
chamikaCN <chamikanandasiri97@gmail.com>
2019-08-18 21:26:30 +0530

    environment building

---
+++ Assets/Obeliskmover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obeliskmover : MonoBehaviour
+{
+
+    float horizontalxMove, horizontalzMove;
+    Rigidbody RB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalxMove = Input.GetAxisRaw("Horizontal");
+        horizontalzMove = Input.GetAxisRaw("Vertical");
+        RB.velocity = new Vector3(horizontalxMove, RB.velocity.y, horizontalzMove);
+    }
+}
