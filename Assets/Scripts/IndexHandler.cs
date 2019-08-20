c1b24c3b9a6c68a772bb54ec8da394beeb9ec69c
chamikaCN <chamikanandasiri97@gmail.com>
2019-08-21 02:12:16 +0530

    ui step 1

---
+++ Assets/Scripts/IndexHandler.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IndexHandler : MonoBehaviour
+{
+    public InputField indexInput;
+    public Button submitButton;
+    public int primeDivider;
+    int indexNumber;
+    int groupID;
+
+    public void Submit()
+    {
+        primeDivider = 13;
+        string indexer = indexInput.text;
+        if(indexer.Length == 6)
+        {
+            indexNumber = int.Parse(indexer);
+            calculateGroup(indexNumber);
+            if (groupID == 1 | groupID == 2 | groupID == 3 | groupID == 4)
+            {
+                PlayerPrefs.SetInt("Group ID", groupID);
+                SceneManager.LoadScene("ARpart");
+            }
+            else
+            {
+                Debug.LogError("invalid index");
+            }
+        }
+        else
+        {
+            Debug.LogError("index should include 6 digits");
+        }
+    }
+
+    void calculateGroup(int index)
+    {
+        switch (index % primeDivider)
+        {
+            case 2:
+                groupID = 1;
+                Debug.Log("you are in team 1");
+                break;
+            case 5:
+                groupID = 2;
+                Debug.Log("you are in team 2");
+                break;
+            case 8:
+                groupID = 3;
+                Debug.Log("you are in team 3");
+                break;
+            case 11:
+                groupID = 4;
+                Debug.Log("you are in team 4");
+                break;
+            default:
+                Debug.Log("invalid pass code");
+                break;
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/TargetHandler.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetHandler : MonoBehaviour
+{
+
+    public Toggle changer;
+    public GameObject item1, item2, item3, item4;
+    int GroupID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        changer.isOn = true;
+        //item1 = GetComponent<Transform>().GetChild(0).gameObject;
+        //item2 = GetComponent<Transform>().GetChild(1).gameObject;
+        //item3 = GetComponent<Transform>().GetChild(2).gameObject;
+        //item4 = GetComponent<Transform>().GetChild(3).gameObject;
+        item1.SetActive(true);
+        item2.SetActive(false);
+        item3.SetActive(false);
+        item4.SetActive(false);
+        GroupID = PlayerPrefs.GetInt("Group ID");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(changer.isOn == true)
+        {
+            Debug.Log("On");
+            item1.SetActive(false);
+            item2.SetActive(true);
+        }
+        else if(changer.isOn == false)
+        {
+            Debug.Log("off");
+            item2.SetActive(false);
+            item1.SetActive(true);
+        }*/
+
+        switch (GroupID)
+        {
+            case 1:
+                Debug.Log(" 1 is On");
+                item1.SetActive(true);
+                item2.SetActive(false);
+                item3.SetActive(false);
+                item4.SetActive(false);
+                break;
+            case 2:
+                Debug.Log(" 2 is On");
+                item1.SetActive(false);
+                item2.SetActive(true);
+                item3.SetActive(false);
+                item4.SetActive(false);
+                break;
+            case 3:
+                Debug.Log(" 3 is On");
+                item1.SetActive(false);
+                item2.SetActive(false);
+                item3.SetActive(true);
+                item4.SetActive(false);
+                break;
+            case 4:
+                Debug.Log(" 4 is On");
+                item1.SetActive(false);
+                item2.SetActive(false);
+                item3.SetActive(false);
+                item4.SetActive(true);
+                break;
+
+        }
+    }
+}
